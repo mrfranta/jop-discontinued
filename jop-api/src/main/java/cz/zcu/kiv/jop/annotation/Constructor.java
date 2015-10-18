fe1cbc179cd5af87755b11606069e571143e2e67
mrfranta <michal.dekany@seznam.cz>
2015-10-18 21:27:51 +0200

    #4 - Added annotation for specifying of used costructor for construction of a generated object.

---
+++ jop-api/src/main/java/cz/zcu/kiv/jop/annotation/Constructor.java
@@ -0,0 +1,20 @@
+package cz.zcu.kiv.jop.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * This annotation may be used case that a generated <em>Object</em> has
+ * multiple constructors to prevent ambiguity which constructor will be used for
+ * <em>Object</em> construction.
+ *
+ * @author Mr.FrAnTA
+ * @since 1.0
+ */
+@Documented
+@Target({ElementType.CONSTRUCTOR})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Constructor {}
