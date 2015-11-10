4c71433a3a6efdc608dadb5f02f2956f619fd5b0
mrfranta <michal.dekany@seznam.cz>
2015-11-11 00:21:10 +0100

    #23 - Implemented abstract class for simple value generator (parameterless).

---
+++ jop-api/src/main/java/cz/zcu/kiv/jop/generator/SimpleValueGenerator.java
@@ -0,0 +1,39 @@
+package cz.zcu.kiv.jop.generator;
+
+import cz.zcu.kiv.jop.annotation.Empty;
+
+/**
+ * Abstract implementation of {@link ValueGenerator} interface which don't need
+ * parameters for generating of values. Also implementations of this class can
+ * be used in {@link cz.zcu.kiv.jop.annotation.generator.CustomValueGenerator
+ * CustomValueGenerator} annotation.
+ *
+ * @author Mr.FrAnTA
+ * @since 1.0
+ *
+ * @param <T> Type of generated values.
+ */
+public abstract class SimpleValueGenerator<T> implements ValueGenerator<T, Empty> {
+
+  /**
+   * Returns generated value according to given parameters.
+   *
+   * @param params the parameters for generation of value (unused).
+   * @return Generated value.
+   * @throws ValueGeneratorException if some error occurs during value
+   *           generation.
+   */
+  public final T getValue(Empty params) throws ValueGeneratorException {
+    return getValue();
+  }
+
+  /**
+   * Returns generated value.
+   *
+   * @return Generated value.
+   * @throws ValueGeneratorException if some error occurs during value
+   *           generation.
+   */
+  public abstract T getValue() throws ValueGeneratorException;
+
+}
