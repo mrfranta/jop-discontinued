2647f24e672a1ac570d27dadf884d6b6bc1ff955
mrfranta <michal.dekany@seznam.cz>
2016-02-05 13:19:13 +0100

    #51 - Adjusted instance matchers to use EmptyParameters annotation.

--- jop-api/src/main/java/cz/zcu/kiv/jop/matcher/SimpleInstanceMatcher.java
+++ jop-api/src/main/java/cz/zcu/kiv/jop/matcher/SimpleInstanceMatcher.java
@@ -1,17 +1,15 @@
 package cz.zcu.kiv.jop.matcher;
 
-import cz.zcu.kiv.jop.annotation.Empty;
+import cz.zcu.kiv.jop.annotation.parameters.EmptyParameters;
 
 /**
- * Abstract implementation of {@link InstanceMatcher} interface which don't need
- * parameters for matching of objects (instances). Also implementations of this
- * class can be used in
- * {@link cz.zcu.kiv.jop.annotation.matcher.CustomInstanceMatcher
- * CustomInstanceMatcher} annotation.
+ * Abstract implementation of {@link InstanceMatcher} interface which don't need parameters for
+ * matching of objects (instances). Also implementations of this class can be used in
+ * {@link cz.zcu.kiv.jop.annotation.matcher.CustomInstanceMatcher CustomInstanceMatcher} annotation.
  *
  * @author Mr.FrAnTA
- * @since 1.0
+ * @since 1.0.0
  */
-public abstract class SimpleInstanceMatcher extends SimpleTypedInstanceMatcher<Object> implements InstanceMatcher<Empty> {
+public abstract class SimpleInstanceMatcher extends SimpleTypedInstanceMatcher<Object> implements InstanceMatcher<EmptyParameters> {
 
 }
--- jop-api/src/main/java/cz/zcu/kiv/jop/matcher/SimpleTypedInstanceMatcher.java
+++ jop-api/src/main/java/cz/zcu/kiv/jop/matcher/SimpleTypedInstanceMatcher.java
@@ -1,34 +1,31 @@
 package cz.zcu.kiv.jop.matcher;
 
-import cz.zcu.kiv.jop.annotation.Empty;
+import cz.zcu.kiv.jop.annotation.parameters.EmptyParameters;
 
 /**
- * Abstract implementation of {@link TypedInstanceMatcher} interface which don't
- * need parameters for matching of objects (instances). Also implementations of
- * this class can be used in
- * {@link cz.zcu.kiv.jop.annotation.matcher.CustomInstanceMatcher
- * CustomInstanceMatcher} annotation.
+ * Abstract implementation of {@link TypedInstanceMatcher} interface which don't need parameters for
+ * matching of objects (instances). Also implementations of this class can be used in
+ * {@link cz.zcu.kiv.jop.annotation.matcher.CustomInstanceMatcher CustomInstanceMatcher} annotation.
  *
  * @author Mr.FrAnTA
- * @since 1.0
+ * @since 1.0.0
  *
  * @param <T> Type of checked objects (instances).
  */
-public abstract class SimpleTypedInstanceMatcher<T> implements TypedInstanceMatcher<T, Empty> {
+public abstract class SimpleTypedInstanceMatcher<T> implements TypedInstanceMatcher<T, EmptyParameters> {
 
   /**
-   * Checks whatever the given object matches custom conditions. The method is
-   * final and serves only as bridge to method which is without parameters -
-   * {@link #matches(Object)}.
+   * Checks whatever the given object matches custom conditions. The method is final and serves only
+   * as bridge to method which is without parameters - {@link #matches(Object)}.
    *
    * @param obj the object (instance) to check.
    * @param params the parameters for matching of given object (unused).
-   * @return <code>true</code> in case that given object matches to conditions;
-   *         <code>false</code> otherwise.
-   * @throws InstanceMatcherException if some error occurs during checking of
-   *           given object or if given parameters are not valid.
+   * @return <code>true</code> in case that given object matches to conditions; <code>false</code>
+   *         otherwise.
+   * @throws InstanceMatcherException if some error occurs during checking of given object or if
+   *           given parameters are not valid.
    */
-  public final boolean matches(T obj, Empty params) {
+  public final boolean matches(T obj, EmptyParameters params) {
     return matches(obj);
   }
 
@@ -36,10 +33,9 @@ public abstract class SimpleTypedInstanceMatcher<T> implements TypedInstanceMatc
    * Checks whatever the given object matches custom conditions.
    *
    * @param obj the object (instance) to check.
-   * @return <code>true</code> in case that given object matches to conditions;
-   *         <code>false</code> otherwise.
-   * @throws InstanceMatcherException if some error occurs during checking of
-   *           given object.
+   * @return <code>true</code> in case that given object matches to conditions; <code>false</code>
+   *         otherwise.
+   * @throws InstanceMatcherException if some error occurs during checking of given object.
    */
   public abstract boolean matches(T obj);
 
