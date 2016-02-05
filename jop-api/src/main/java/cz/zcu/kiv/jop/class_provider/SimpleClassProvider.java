fbca5a1d8a9c935fea03376d8e4364fc09ebd03f
mrfranta <michal.dekany@seznam.cz>
2016-02-05 13:09:55 +0100

    #51 - Adjusted simple class providers to use new annotation for empty parameters.

--- jop-api/src/main/java/cz/zcu/kiv/jop/class_provider/SimpleClassProvider.java
+++ jop-api/src/main/java/cz/zcu/kiv/jop/class_provider/SimpleClassProvider.java
@@ -1,30 +1,27 @@
 package cz.zcu.kiv.jop.class_provider;
 
-import cz.zcu.kiv.jop.annotation.Empty;
+import cz.zcu.kiv.jop.annotation.parameters.EmptyParameters;
 
 /**
- * Abstract implementation of {@link ClassProvider} interface which don't need
- * parameters for providing of classes. Also implementations of this class can
- * be used in
- * {@link cz.zcu.kiv.jop.annotation.class_provider.CustomClassProvider
- * CustomClassProvider} annotation.
+ * Abstract implementation of {@link ClassProvider} interface which don't need parameters for
+ * providing of classes. Also implementations of this class can be used in
+ * {@link cz.zcu.kiv.jop.annotation.class_provider.CustomClassProvider CustomClassProvider}
+ * annotation.
  *
  * @author Mr.FrAnTA
- * @since 1.0
+ * @since 1.0.0
  */
-public abstract class SimpleClassProvider implements ClassProvider<Empty> {
+public abstract class SimpleClassProvider implements ClassProvider<EmptyParameters> {
 
   /**
-   * Provides the the class type according to given parameters. This method is
-   * final and serves only as bridge to method which is without parameters -
-   * {@link #get()}.
+   * Provides the the class type according to given parameters. This method is final and serves only
+   * as bridge to method which is without parameters - {@link #get()}.
    *
    * @param params the parameters for choice of provided class (unused).
    * @return Provided class type.
-   * @throws ClassProviderException if some error occurs during providing of
-   *           class type.
+   * @throws ClassProviderException if some error occurs during providing of class type.
    */
-  public final Class<?> get(Empty params) throws ClassProviderException {
+  public final Class<?> get(EmptyParameters params) throws ClassProviderException {
     return get();
   }
 
@@ -32,8 +29,7 @@ public abstract class SimpleClassProvider implements ClassProvider<Empty> {
    * Provides the the class type.
    *
    * @return Provided class type.
-   * @throws ClassProviderException if some error occurs during providing of
-   *           class type.
+   * @throws ClassProviderException if some error occurs during providing of class type.
    */
   public abstract Class<?> get() throws ClassProviderException;
 
--- jop-api/src/main/java/cz/zcu/kiv/jop/class_provider/SimpleTypedClassProvider.java
+++ jop-api/src/main/java/cz/zcu/kiv/jop/class_provider/SimpleTypedClassProvider.java
@@ -1,33 +1,30 @@
 package cz.zcu.kiv.jop.class_provider;
 
-import cz.zcu.kiv.jop.annotation.Empty;
+import cz.zcu.kiv.jop.annotation.parameters.EmptyParameters;
 
 /**
- * Abstract implementation of {@link TypedClassProvider} interface which don't
- * need parameters for providing of classes. Also implementations of this class
- * can be used in
- * {@link cz.zcu.kiv.jop.annotation.class_provider.CustomClassProvider
- * CustomClassProvider} annotation.
+ * Abstract implementation of {@link TypedClassProvider} interface which don't need parameters for
+ * providing of classes. Also implementations of this class can be used in
+ * {@link cz.zcu.kiv.jop.annotation.class_provider.CustomClassProvider CustomClassProvider}
+ * annotation.
  *
  * @author Mr.FrAnTA
- * @since 1.0
+ * @since 1.0.0
  *
- * @param <T> Type for which will be provided the class types. The provided
- *          class types can be also for types which extends the defined type.
+ * @param <T> Type for which will be provided the class types. The provided class types can be also
+ *          for types which extends the defined type.
  */
-public abstract class SimpleTypedClassProvider<T> implements TypedClassProvider<T, Empty> {
+public abstract class SimpleTypedClassProvider<T> implements TypedClassProvider<T, EmptyParameters> {
 
   /**
-   * Provides the the class type according to given parameters. This method is
-   * final and serves only as bridge to method which is without parameters -
-   * {@link #get()}.
+   * Provides the the class type according to given parameters. This method is final and serves only
+   * as bridge to method which is without parameters - {@link #get()}.
    *
    * @param params the parameters for choice of provided class (unused).
    * @return Provided class type.
-   * @throws ClassProviderException if some error occurs during providing of
-   *           class type.
+   * @throws ClassProviderException if some error occurs during providing of class type.
    */
-  public final Class<? extends T> get(Empty params) throws ClassProviderException {
+  public final Class<? extends T> get(EmptyParameters params) throws ClassProviderException {
     return get();
   }
 
@@ -35,8 +32,7 @@ public abstract class SimpleTypedClassProvider<T> implements TypedClassProvider<
    * Provides the the class type.
    *
    * @return Provided class type.
-   * @throws ClassProviderException if some error occurs during providing of
-   *           class type.
+   * @throws ClassProviderException if some error occurs during providing of class type.
    */
   public abstract Class<? extends T> get() throws ClassProviderException;
 
