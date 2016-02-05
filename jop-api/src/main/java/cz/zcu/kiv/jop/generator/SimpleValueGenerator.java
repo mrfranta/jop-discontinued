c146391f97acec3e042d2e8bf4e8f4e524908f92
mrfranta <michal.dekany@seznam.cz>
2016-02-05 13:13:54 +0100

    #51 - Adjusted simple value generator to use empty parameters annotation instead of older one.

--- jop-api/src/main/java/cz/zcu/kiv/jop/generator/SimpleValueGenerator.java
+++ jop-api/src/main/java/cz/zcu/kiv/jop/generator/SimpleValueGenerator.java
@@ -1,29 +1,27 @@
 package cz.zcu.kiv.jop.generator;
 
-import cz.zcu.kiv.jop.annotation.Empty;
+import cz.zcu.kiv.jop.annotation.parameters.EmptyParameters;
 
 /**
- * Abstract implementation of {@link ValueGenerator} interface which don't need
- * parameters for generating of values. Also implementations of this class can
- * be used in {@link cz.zcu.kiv.jop.annotation.generator.CustomValueGenerator
- * CustomValueGenerator} annotation.
+ * Abstract implementation of {@link ValueGenerator} interface which don't need parameters for
+ * generating of values. Also implementations of this class can be used in
+ * {@link cz.zcu.kiv.jop.annotation.generator.CustomValueGenerator CustomValueGenerator} annotation.
  *
  * @author Mr.FrAnTA
- * @since 1.0
+ * @since 1.0.0
  *
  * @param <T> Type of generated values.
  */
-public abstract class SimpleValueGenerator<T> implements ValueGenerator<T, Empty> {
+public abstract class SimpleValueGenerator<T> implements ValueGenerator<T, EmptyParameters> {
 
   /**
    * Returns generated value according to given parameters.
    *
    * @param params the parameters for generation of value (unused).
    * @return Generated value.
-   * @throws ValueGeneratorException if some error occurs during value
-   *           generation.
+   * @throws ValueGeneratorException if some error occurs during value generation.
    */
-  public final T getValue(Empty params) throws ValueGeneratorException {
+  public final T getValue(EmptyParameters params) throws ValueGeneratorException {
     return getValue();
   }
 
@@ -31,8 +29,7 @@ public abstract class SimpleValueGenerator<T> implements ValueGenerator<T, Empty
    * Returns generated value.
    *
    * @return Generated value.
-   * @throws ValueGeneratorException if some error occurs during value
-   *           generation.
+   * @throws ValueGeneratorException if some error occurs during value generation.
    */
   public abstract T getValue() throws ValueGeneratorException;
 
