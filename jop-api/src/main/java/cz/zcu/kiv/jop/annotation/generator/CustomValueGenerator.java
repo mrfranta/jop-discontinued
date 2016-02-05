0ba5900b74f85993bf0618db7a4fa985e538d11e
mrfranta <michal.dekany@seznam.cz>
2016-02-05 12:33:20 +0100

    #51 - Adjusted custom value generator annotation.

--- jop-api/src/main/java/cz/zcu/kiv/jop/annotation/generator/CustomValueGenerator.java
+++ jop-api/src/main/java/cz/zcu/kiv/jop/annotation/generator/CustomValueGenerator.java
@@ -1,24 +1,22 @@
 package cz.zcu.kiv.jop.annotation.generator;
 
-import cz.zcu.kiv.jop.annotation.Empty;
 import cz.zcu.kiv.jop.generator.ValueGenerator;
 
 /**
- * This annotation marks property for which will be generated value using
- * specific {@link ValueGenerator value generator}. The value generator cannot
- * support parameters (only supported is {@link Empty} annotation for empty
- * parameters).
+ * This annotation marks property for which will be generated value using specific
+ * {@link ValueGenerator value generator}. The parameters for given value generator may be set in
+ * separated annotation for property.
  *
  * @see ValueGenerator
  *
  * @author Mr.FrAnTA
- * @since 1.0
+ * @since 1.0.0
  */
 public @interface CustomValueGenerator {
 
   /**
    * Required parameter for value generator which provides generated value.
    */
-  public Class<? extends ValueGenerator<?, Empty>> value();
+  public Class<? extends ValueGenerator<?, ?>> value();
 
 }
