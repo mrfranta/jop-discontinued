11bc87a27a8f780b3bfb2551ceaa186c73484947
mrfranta <michal.dekany@seznam.cz>
2016-02-05 13:10:46 +0100

    #57 - Adjusted javadoc for class providers.

--- jop-api/src/main/java/cz/zcu/kiv/jop/class_provider/ClassProvider.java
+++ jop-api/src/main/java/cz/zcu/kiv/jop/class_provider/ClassProvider.java
@@ -3,18 +3,16 @@ package cz.zcu.kiv.jop.class_provider;
 import java.lang.annotation.Annotation;
 
 /**
- * The common interface for all class providers. It may be used for getting of
- * specific class type that is used for new instance creation in case that
- * declaring type of field (into which will be new instance created) is
- * interface or abstract class.
+ * The common interface for all class providers. It may be used for getting of specific class type
+ * that is used for new instance creation in case that declaring type of field (into which will be
+ * new instance created) is interface or abstract class.
  * <p>
- * Type parameter is for annotation type of parameters for class provider. The
- * parameters for {@link #get method} which provides the class type are always
- * stored in annotation (for example: by which is annotated the field for which
- * will be provided the class type).
+ * Type parameter is for annotation type of parameters for class provider. The parameters for
+ * {@link #get method} which provides the class type are always stored in annotation (for example:
+ * by which is annotated the field for which will be provided the class type).
  *
  * @author Mr.FrAnTA
- * @since 1.0
+ * @since 1.0.0
  *
  * @param <P> Annotation type of class provider parameters.
  */
@@ -25,8 +23,8 @@ public interface ClassProvider<P extends Annotation> {
    *
    * @param params the parameters for choice of provided class.
    * @return Provided class type.
-   * @throws ClassProviderException if some error occurs during providing of
-   *           class type or if given parameters are not valid.
+   * @throws ClassProviderException if some error occurs during providing of class type or if given
+   *           parameters are not valid.
    */
   public Class<?> get(P params) throws ClassProviderException;
 
--- jop-api/src/main/java/cz/zcu/kiv/jop/class_provider/ClassProviderFactory.java
+++ jop-api/src/main/java/cz/zcu/kiv/jop/class_provider/ClassProviderFactory.java
@@ -6,32 +6,28 @@ import cz.zcu.kiv.jop.factory.BindingFactory;
 import cz.zcu.kiv.jop.factory.FactoryException;
 
 /**
- * The interface for factory which serves for creation of {@link ClassProvider
- * ClassProviders}. Class providers should be created primary by this factory.
+ * The interface for factory which serves for creation of {@link ClassProvider ClassProviders}.
+ * Class providers should be created primary by this factory.
  *
  * @author Mr.FrAnTA
- * @since 1.0
+ * @since 1.0.0
  */
 public interface ClassProviderFactory extends BindingFactory<ClassProvider<?>> {
 
   /**
-   * Returns created instance of bound implementation of class provider for
-   * given annotation. If the given class is annotated by
-   * {@link javax.inject.Singleton Singleton} annotation it will be cached so
-   * the returned instances will be singletons. Otherwise the factory always
-   * creates new instance for each call of this method.
+   * Returns created instance of bound implementation of class provider for given annotation. If the
+   * given class is annotated by {@link javax.inject.Singleton Singleton} annotation it will be
+   * cached so the returned instances will be singletons. Otherwise the factory always creates new
+   * instance for each call of this method.
    * <p>
-   * In case that there is no bound implementation of class provider for given
-   * annotation the exception may be thrown - the <code>null</code> value should
-   * not be returned.
+   * In case that there is no bound implementation of class provider for given annotation the
+   * exception may be thrown - the <code>null</code> value should not be returned.
    *
-   * @param annotation the annotation for which will be created bound instance
-   *          of class provider implementation.
-   * @return Created instance of class provider implementation which was bound
-   *         to given annotation.
-   * @throws FactoryException if some error occurs during creation of (new)
-   *           instance of given class or if no implementation was bound to
-   *           given annotation.
+   * @param annotation the annotation for which will be created bound instance of class provider
+   *          implementation.
+   * @return Created instance of class provider implementation which was bound to given annotation.
+   * @throws FactoryException if some error occurs during creation of (new) instance of given class
+   *           or if no implementation was bound to given annotation.
    */
   public <A extends Annotation> ClassProvider<A> createInstance(A annotation) throws FactoryException;
 
