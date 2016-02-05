e272cb025b9e5a975f0e1afe8294177f7a5a4683
mrfranta <michal.dekany@seznam.cz>
2016-02-05 13:21:41 +0100

    #51 - Adjusted annotation for custom instance matcher.

--- jop-api/src/main/java/cz/zcu/kiv/jop/annotation/matcher/CustomInstanceMatcher.java
+++ jop-api/src/main/java/cz/zcu/kiv/jop/annotation/matcher/CustomInstanceMatcher.java
@@ -6,21 +6,21 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import cz.zcu.kiv.jop.annotation.Empty;
+import cz.zcu.kiv.jop.annotation.CustomAnnotation;
 import cz.zcu.kiv.jop.matcher.TypedInstanceMatcher;
 
 /**
- * Objects (instances) matcher annotation which determines the specific instance
- * of {@link TypedInstanceMatcher matcher} which decides which already generated
- * object (instance) can be set into populated field. Instance matcher cannot
- * support parameters (only supported is {@link Empty} annotation for empty
- * parameters).
+ * Objects (instances) matcher annotation which determines the specific instance of
+ * {@link TypedInstanceMatcher matcher} which decides which already generated object (instance) can
+ * be set into populated field. The parameters for given instance matcher may be set in separated
+ * annotation for property.
  *
  * @see TypedInstanceMatcher
  *
  * @author Mr.FrAnTA
- * @since 1.0
+ * @since 1.0.0
  */
+@CustomAnnotation
 @InstanceMatcherAnnotation
 @Documented
 @Target({ElementType.FIELD, ElementType.PARAMETER})
@@ -28,9 +28,9 @@ import cz.zcu.kiv.jop.matcher.TypedInstanceMatcher;
 public @interface CustomInstanceMatcher {
 
   /**
-   * Required parameter for instance matcher which decides which already
-   * generated object (instance) can be set into populated field.
+   * Required parameter for instance matcher which decides which already generated object (instance)
+   * can be set into populated field.
    */
-  public Class<? extends TypedInstanceMatcher<?, Empty>> value();
+  public Class<? extends TypedInstanceMatcher<?, ?>> value();
 
 }
