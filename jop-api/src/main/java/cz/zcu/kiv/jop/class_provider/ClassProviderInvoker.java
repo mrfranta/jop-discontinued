75f43c66c20ec9904ad09257db7060fb05cd384b
mrfranta <michal.dekany@seznam.cz>
2016-03-10 01:33:31 +0100

    #73 - Adjusted interlayers for class providers and value generators - added support for multiple invocations.

--- jop-api/src/main/java/cz/zcu/kiv/jop/class_provider/ClassProviderInvoker.java
+++ jop-api/src/main/java/cz/zcu/kiv/jop/class_provider/ClassProviderInvoker.java
@@ -5,8 +5,8 @@ import cz.zcu.kiv.jop.property.Property;
 /**
  * Interface which serves as interlayer between the class providers and another parts of this
  * library. Implementation of this interface should analyze the annotations of given property and
- * depending on them should choose and invoke some class provider. After invocation is returned the
- * result of chosen class provider.
+ * then should choose and invoke some class provider. After invocation is returned the result of
+ * chosen class provider. There is also support of multiple invocations of same class provider.
  *
  * @author Mr.FrAnTA
  * @since 1.0.0
@@ -21,6 +21,17 @@ public interface ClassProviderInvoker {
    * @return The result of class provider invocation.
    * @throws ClassProviderException If some error occurs during class provider invocation.
    */
-  public Class<?> getClass(Property<?> property) throws ClassProviderException;
+  public Class<?> get(Property<?> property) throws ClassProviderException;
 
+  /**
+   * Analyzes annotations for given property and then choose the proper class provider. The chosen
+   * class provider is invoked multiple times (parameter <code>count</code>) and then array of
+   * invocation results is returned.
+   *
+   * @param property the property for which will be invoked the proper class provider.
+   * @param count the number of class provider invocations.
+   * @return Array of results of class provider invocations.
+   * @throws ClassProviderException If some error occurs during class provider invocation.
+   */
+  public Class<?>[] get(Property<?> property, int count) throws ClassProviderException;
 }
--- jop-api/src/main/java/cz/zcu/kiv/jop/generator/ValueGeneratorInvoker.java
+++ jop-api/src/main/java/cz/zcu/kiv/jop/generator/ValueGeneratorInvoker.java
@@ -5,8 +5,8 @@ import cz.zcu.kiv.jop.property.Property;
 /**
  * Interface which serves as interlayer between the value generators and another parts of this
  * library. Implementation of this interface should analyze the annotations of given property and
- * depending on them should choose and invoke some value generator. After invocation is returned the
- * result of chosen value generator.
+ * then should choose and invoke some value generator. After invocation is returned the result of
+ * chosen value generator. There is also support of multiple invocations of same value generator.
  *
  * @author Mr.FrAnTA
  * @since 1.0.0
@@ -23,4 +23,15 @@ public interface ValueGeneratorInvoker {
    */
   public Object getValue(Property<?> property) throws ValueGeneratorException;
 
+  /**
+   * Analyzes annotations for given property and then choose the proper value generator. The chosen
+   * value generator is invoked multiple times (parameter <code>count</code>) and then array of
+   * invocation results is returned.
+   *
+   * @param property the property for which will be invoked the proper value generator.
+   * @param count the number of value generator invocations.
+   * @return Array of results of value generator invocations.
+   * @throws ValueGeneratorException If some error occurs during value generator invocation.
+   */
+  public Object[] getValue(Property<?> property, int count) throws ValueGeneratorException;
 }
