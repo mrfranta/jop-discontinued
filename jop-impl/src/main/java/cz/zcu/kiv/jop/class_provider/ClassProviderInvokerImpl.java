18be0e5ce2244bb367bf2828d137af14b42f21cc
mrfranta <michal.dekany@seznam.cz>
2016-03-10 01:36:11 +0100

    #76 - Implemented interlayers for value generators and class providers.

---
+++ jop-impl/src/main/java/cz/zcu/kiv/jop/class_provider/ClassProviderInvokerImpl.java
@@ -0,0 +1,198 @@
+package cz.zcu.kiv.jop.class_provider;
+
+import java.lang.annotation.Annotation;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import cz.zcu.kiv.jop.annotation.CustomAnnotation;
+import cz.zcu.kiv.jop.annotation.class_provider.ClassProviderAnnotation;
+import cz.zcu.kiv.jop.annotation.class_provider.CustomClassProvider;
+import cz.zcu.kiv.jop.annotation.parameters.CustomParameters;
+import cz.zcu.kiv.jop.annotation.parameters.EmptyParameters;
+import cz.zcu.kiv.jop.factory.FactoryException;
+import cz.zcu.kiv.jop.property.Property;
+import cz.zcu.kiv.jop.util.AnnotationUtils;
+import cz.zcu.kiv.jop.util.ReflectionUtils;
+
+/**
+ * Implementation of class provider invoker (interlayer between another layers and class providers)
+ * which analyzes the annotations of property and then choose and invoke some class provider. After
+ * invocation is returned the result of chosen class provider. There is also support of multiple
+ * invocations of same class provider.
+ *
+ * @author Mr.FrAnTA
+ * @since 1.0.0
+ */
+@Singleton
+public class ClassProviderInvokerImpl implements ClassProviderInvoker {
+
+  /**
+   * {@inheritDoc}
+   */
+  public Class<?> get(Property<?> property) throws ClassProviderException {
+    return get(property, 1)[0]; // returns only one provided class
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public Class<?>[] get(Property<?> property, int count) throws ClassProviderException {
+    if (property == null) {
+      throw new ClassProviderException("Property cannot be null");
+    }
+
+    if (count <= 0) {
+      throw new ClassProviderException("Number of invocations has to be greater than 0");
+    }
+
+    Annotation[] annotations = AnnotationUtils.getAnnotatedAnnotations(property, ClassProviderAnnotation.class);
+    if (annotations == null || annotations.length == 0) {
+      throw new ClassProviderException("No such class provider annotation");
+    }
+
+    if (annotations.length > 1) {
+      throw new ClassProviderException("Too many class provider annotations");
+    }
+
+    Annotation params = annotations[0];
+    ClassProvider<Annotation> classProvider = null;
+    if (AnnotationUtils.isAnnotatedAnnotation(params, CustomAnnotation.class)) {
+      // custom class provider handling
+      classProvider = getCustomClassProvider(params);
+      params = getCustomClassProviderParams(property, classProvider.getClass(), params.annotationType());
+    }
+    else {
+      // class providers bound in class provider factory
+      classProvider = getBoundClassProvider(params);
+    }
+
+    Class<?>[] classes = new Class<?>[count];
+    for (int i = 0; i < count; i++) {
+      classes[i] = classProvider.get(params);
+    }
+
+    return classes;
+  }
+
+  /**
+   * Returns instance of class provider for given custom annotation. There is supported only
+   * {@link CustomClassProvider} annotation.
+   *
+   * @param annotation the custom class provider annotation.
+   * @return Instance of custom class provider.
+   * @throws ClassProviderException If the given annotation isn't supported or if some problem
+   *           occurs during custom class provider creation.
+   */
+  @SuppressWarnings("unchecked")
+  protected ClassProvider<Annotation> getCustomClassProvider(Annotation annotation) throws ClassProviderException {
+    if (!(annotation instanceof CustomClassProvider)) {
+      throw new ClassProviderException("Unsupported custom annotation: " + annotation.annotationType().getName());
+    }
+
+    Class<? extends ClassProvider<?>> customClassProvider = ((CustomClassProvider)annotation).value();
+    try {
+      return (ClassProvider<Annotation>)classProviderFactory.createInstance(customClassProvider);
+    }
+    catch (FactoryException exc) {
+      throw new ClassProviderException(exc);
+    }
+  }
+
+  /**
+   * Analyzes the annotations of given property and returns annotation for custom parameters of
+   * custom class provider.
+   *
+   * @param property the property for which will be returned custom parameters.
+   * @param customClassProvider the class type of custom class provider.
+   * @param customAnnotation the class type of custom class provider annotation.
+   * @return The annotation for custom parameters of class provider.
+   * @throws ClassProviderException If custom parameters for custom class provider cannot be
+   *           obtained.
+   */
+  protected Annotation getCustomClassProviderParams(Property<?> property, Class<?> customClassProvider, Class<?> customAnnotation) throws ClassProviderException {
+    // empty parameters
+    if (ReflectionUtils.getMethod(customClassProvider, "get", EmptyParameters.class) != null) {
+      return AnnotationUtils.getAnnotationProxy(EmptyParameters.class, null);
+    }
+
+    // custom parameters
+
+    Annotation[] customParameters = AnnotationUtils.getAnnotatedAnnotations(property, CustomParameters.class);
+    if (customParameters.length == 0) {
+      throw new ClassProviderException("Missing custom parameters for class provider: " + customClassProvider.getName());
+    }
+
+    Annotation customParams = null;
+
+    // we have to find correct parameters
+    if (customParameters.length > 1) {
+      for (int i = 0; i < customParameters.length; i++) {
+        // find matching custom parameters via CustomParameters#value()
+        CustomParameters customParamsMarker = customParameters[i].annotationType().getAnnotation(CustomParameters.class);
+        Class<? extends Annotation> target = customParamsMarker.value();
+        if (target.equals(customAnnotation)) {
+          if (customParams != null) { // too many annotations
+            customParams = null;
+            break;
+          }
+          customParams = customParameters[i];
+        }
+      }
+
+      // no matching parameters not found
+      if (customParams == null) {
+        for (int i = 0; i < customParameters.length; i++) {
+          if (ReflectionUtils.getMethod(customClassProvider, "get", customParameters[i].annotationType()) != null) {
+            // we can easily break in this point because we cannot have same annotation multiple times
+            return customParameters[i];
+          }
+        }
+      }
+    }
+    else {
+      customParams = customParameters[0];
+    }
+
+    // checks compatibility of custom parameters
+    Class<?> customParamsType = customParams.annotationType();
+    if (ReflectionUtils.getMethod(customClassProvider, "get", customParamsType) != null) {
+      return customParams;
+    }
+    else {
+      throw new ClassProviderException("Incompatible custom parameters: " + customParamsType.getName() + " for class provider: "
+          + customClassProvider.getName());
+    }
+  }
+
+  /**
+   * Returns instance of class provider bound for given annotation in {@link ClassProviderFactory}.
+   *
+   * @param annotation the parameters annotation for which will be returned the class provider.
+   * @return Bound class provider for given annotation.
+   * @throws ClassProviderException If some error occurs during obtaining of bound class provider.
+   */
+  protected ClassProvider<Annotation> getBoundClassProvider(Annotation annotation) throws ClassProviderException {
+    try {
+      return classProviderFactory.createInstance(annotation);
+    }
+    catch (FactoryException exc) {
+      throw new ClassProviderException(exc);
+    }
+  }
+
+  //----- Injection part ------------------------------------------------------
+
+  /** Binding factory for creating/providing instances of class providers. */
+  protected ClassProviderFactory classProviderFactory;
+
+  /**
+   * Sets (injects) binding factory for creating/providing instances of class providers.
+   *
+   * @param classProviderFactory the class provider factory to set (inject).
+   */
+  @Inject
+  public final void setClassProviderFactory(ClassProviderFactory classProviderFactory) {
+    this.classProviderFactory = classProviderFactory;
+  }
+}
---
+++ jop-impl/src/main/java/cz/zcu/kiv/jop/generator/ValueGeneratorInvokerImpl.java
@@ -0,0 +1,199 @@
+package cz.zcu.kiv.jop.generator;
+
+import java.lang.annotation.Annotation;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import cz.zcu.kiv.jop.annotation.CustomAnnotation;
+import cz.zcu.kiv.jop.annotation.generator.CustomValueGenerator;
+import cz.zcu.kiv.jop.annotation.generator.GeneratorAnnotation;
+import cz.zcu.kiv.jop.annotation.parameters.CustomParameters;
+import cz.zcu.kiv.jop.annotation.parameters.EmptyParameters;
+import cz.zcu.kiv.jop.factory.FactoryException;
+import cz.zcu.kiv.jop.property.Property;
+import cz.zcu.kiv.jop.util.AnnotationUtils;
+import cz.zcu.kiv.jop.util.ReflectionUtils;
+
+/**
+ * Implementation of value generator invoker (interlayer between another layers and value
+ * generators) which analyzes the annotations of property and then choose and invoke some value
+ * generator. After invocation is returned the result of chosen value generator. There is also
+ * support of multiple invocations of same value generator.
+ *
+ * @author Mr.FrAnTA
+ * @since 1.0.0
+ */
+@Singleton
+public class ValueGeneratorInvokerImpl implements ValueGeneratorInvoker {
+
+  /**
+   * {@inheritDoc}
+   */
+  public Object getValue(Property<?> property) throws ValueGeneratorException {
+    return getValue(property, 1)[0]; // returns only one generated value
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public Object[] getValue(Property<?> property, int count) throws ValueGeneratorException {
+    if (property == null) {
+      throw new ValueGeneratorException("Property cannot be null");
+    }
+
+    if (count <= 0) {
+      throw new ValueGeneratorException("Number of invocations has to be greater than 0");
+    }
+
+    Annotation[] annotations = AnnotationUtils.getAnnotatedAnnotations(property, GeneratorAnnotation.class);
+    if (annotations == null || annotations.length == 0) {
+      throw new ValueGeneratorException("No such value generator annotation");
+    }
+
+    if (annotations.length > 1) {
+      throw new ValueGeneratorException("Too many value generator annotations");
+    }
+
+    Annotation params = annotations[0];
+    ValueGenerator<?, Annotation> valueGenerator = null;
+    if (AnnotationUtils.isAnnotatedAnnotation(params, CustomAnnotation.class)) {
+      // custom value generator handling
+      valueGenerator = getCustomValueGenerator(params);
+      params = getCustomValueGeneratorParams(property, valueGenerator.getClass(), params.annotationType());
+    }
+    else {
+      // value generators bound in value generator factory
+      valueGenerator = getBoundValueGenerator(params);
+    }
+
+    Object[] values = new Object[count];
+    for (int i = 0; i < count; i++) {
+      values[i] = valueGenerator.getValue(params);
+    }
+
+    return values;
+  }
+
+  /**
+   * Returns instance of value generator for given custom annotation. There is supported only
+   * {@link CustomValueGenerator} annotation.
+   *
+   * @param annotation the custom value generator annotation.
+   * @return Instance of custom value generator.
+   * @throws ValueGeneratorException If the given annotation isn't supported or if some problem
+   *           occurs during custom value generator creation.
+   */
+  @SuppressWarnings("unchecked")
+  protected ValueGenerator<?, Annotation> getCustomValueGenerator(Annotation annotation) throws ValueGeneratorException {
+    if (!(annotation instanceof CustomValueGenerator)) {
+      throw new ValueGeneratorException("Unsupported custom annotation: " + annotation.annotationType().getName());
+    }
+
+    Class<? extends ValueGenerator<?, ?>> customValueGenerator = ((CustomValueGenerator)annotation).value();
+    try {
+      return (ValueGenerator<?, Annotation>)valueGeneratorFactory.createInstance(customValueGenerator);
+    }
+    catch (FactoryException exc) {
+      throw new ValueGeneratorException(exc);
+    }
+  }
+
+  /**
+   * Analyzes the annotations of given property and returns annotation for custom parameters of
+   * custom value generator.
+   *
+   * @param property the property for which will be returned custom parameters.
+   * @param customValueGenerator the class type of custom value generator.
+   * @param customAnnotation the class type of custom value generator annotation.
+   * @return The annotation for custom parameters of value generator.
+   * @throws ValueGeneratorException If custom parameters for custom value generator cannot be
+   *           obtained.
+   */
+  protected Annotation getCustomValueGeneratorParams(Property<?> property, Class<?> customValueGenerator, Class<?> customAnnotation) throws ValueGeneratorException {
+    // empty parameters
+    if (ReflectionUtils.getMethod(customValueGenerator, "get", EmptyParameters.class) != null) {
+      return AnnotationUtils.getAnnotationProxy(EmptyParameters.class, null);
+    }
+
+    // custom parameters
+
+    Annotation[] customParameters = AnnotationUtils.getAnnotatedAnnotations(property, CustomParameters.class);
+    if (customParameters.length == 0) {
+      throw new ValueGeneratorException("Missing custom parameters for value generator: " + customValueGenerator.getName());
+    }
+
+    Annotation customParams = null;
+
+    // we have to find correct parameters
+    if (customParameters.length > 1) {
+      for (int i = 0; i < customParameters.length; i++) {
+        // find matching custom parameters via CustomParameters#value()
+        CustomParameters customParamsMarker = customParameters[i].annotationType().getAnnotation(CustomParameters.class);
+        Class<? extends Annotation> target = customParamsMarker.value();
+        if (target.equals(customAnnotation)) {
+          if (customParams != null) { // too many annotations
+            customParams = null;
+            break;
+          }
+          customParams = customParameters[i];
+        }
+      }
+
+      // no matching parameters not found
+      if (customParams == null) {
+        for (int i = 0; i < customParameters.length; i++) {
+          if (ReflectionUtils.getMethod(customValueGenerator, "get", customParameters[i].annotationType()) != null) {
+            // we can easily break in this point because we cannot have same annotation multiple times
+            return customParameters[i];
+          }
+        }
+      }
+    }
+    else {
+      customParams = customParameters[0];
+    }
+
+    // checks compatibility of custom parameters
+    Class<?> customParamsType = customParams.annotationType();
+    if (ReflectionUtils.getMethod(customValueGenerator, "get", customParamsType) != null) {
+      return customParams;
+    }
+    else {
+      throw new ValueGeneratorException("Incompatible custom parameters: " + customParamsType.getName() + " for value generator: "
+          + customValueGenerator.getName());
+    }
+  }
+
+  /**
+   * Returns instance of value generator bound for given annotation in {@link ValueGeneratorFactory}
+   * .
+   *
+   * @param annotation the parameters annotation for which will be returned the value generator.
+   * @return Bound value generator for given annotation.
+   * @throws ValueGeneratorException If some error occurs during obtaining of bound value generator.
+   */
+  protected ValueGenerator<?, Annotation> getBoundValueGenerator(Annotation annotation) throws ValueGeneratorException {
+    try {
+      return valueGeneratorFactory.createInstance(annotation);
+    }
+    catch (FactoryException exc) {
+      throw new ValueGeneratorException(exc);
+    }
+  }
+
+  //----- Injection part ------------------------------------------------------
+
+  /** Binding factory for creating/providing instances of value generators. */
+  protected ValueGeneratorFactory valueGeneratorFactory;
+
+  /**
+   * Sets (injects) binding factory for creating/providing instances of value generators.
+   *
+   * @param valueGeneratorFactory the value generator factory to set (inject).
+   */
+  @Inject
+  public final void setValueGeneratorFactory(ValueGeneratorFactory valueGeneratorFactory) {
+    this.valueGeneratorFactory = valueGeneratorFactory;
+  }
+}
